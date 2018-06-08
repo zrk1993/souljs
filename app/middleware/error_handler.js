ad033815a3cb40a330c8ac36d54782631fb26bad
仁焜 <1051455824@qq.com>
2018-06-08 23:39:22 +0800

    add

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -4,20 +4,15 @@
  *
  * @return {function} Koa middleware.
  */
-module.exports = function _() {
-  return async (ctx, next) => {
-    try {
-      console.log(1);
-      const a = await next();
-      a.catch(() => {
-        debugger;
-      });
-      console.log(2);
-      debugger;
-    } catch (err) {
-      debugger;
-      ctx.app.emit('error', err, ctx);
-    }
-    console.log(3);
-  };
+module.exports = async (ctx, next) => {
+  try {
+    console.log(1);
+    await next();
+    console.log(2);
+    debugger;
+  } catch (err) {
+    debugger;
+    ctx.app.emit('error', err, ctx);
+  }
+  console.log(3);
 };
