de2ebf80fc067fe3d0c71d7f7deaef46dad33128
Your Name <you@example.com>
2019-01-17 19:43:21 +0800

    add

--- lib/interfaces/request-params.ts
+++ lib/interfaces/request-params.ts
@@ -7,20 +7,3 @@ export interface IResponse extends Koa.Response {}
 export interface INext extends Function {}
 
 export interface ICtx extends Koa.Context {}
-
-export interface ISession {
-    [propName: string]: any;
-}
-
-export interface IQuery {
-    [propName: string]: any;
-}
-
-export interface IParam {
-    [propName: string]: any;
-}
-
-export interface IBody {
-    [propName: string]: any;
-}
-
